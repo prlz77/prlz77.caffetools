c6d1ba09dc14022c6e9057c3e03ce5b7b2249d87
prlz77 <pau.rodri1@gmail.com>
2017-03-03 12:13:26 +0100

    small fixes

--- parsers/parse_log.py
+++ parsers/parse_log.py
@@ -21,11 +21,11 @@ def parse_caffe(path, output_fields=['accuracy']):
         for l in lines:
             if 'Iteration' in l and  'Testing net' in l:
                 test = True
-                iteration = int(l.split('Iteration ')[1].split(',')[0])
+                iteration = int(l.split('Iteration ')[1].split(',')[0].split(' ')[0])
                 test_log.append({'iter':iteration})
             elif 'Iteration' in l:
                 test = False
-                iteration = int(l.split('Iteration ')[1].split(',')[0])
+                iteration = int(l.split('Iteration ')[1].split(',')[0].split(' ')[0])
                 train_log.append({'iter':iteration})
                 if 'loss' in l:
                     loss = float(l.split('loss = ')[-1])
@@ -33,12 +33,12 @@ def parse_caffe(path, output_fields=['accuracy']):
             elif test:
                 found_field = False
                 for field in output_fields:
-                    if field in l:
+                    if field + " =" in l:
                         accuracy = float(l.split('%s = '%field)[-1])
                         test_log[-1][field] = accuracy
                         found_field = True
                         break
-                if not found_field and 'loss' in l:
+                if not found_field and 'loss =' in l:
                     loss = float(l.split('loss = ')[-1].split(' ')[0])
                     test_log[-1]['loss'] = loss
 
