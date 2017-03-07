495cc9be05ffa067f04c9d15bedb6318b5a200d3
prlz77 <pau.rodri1@gmail.com>
2017-03-07 13:12:16 +0100

    more robust parsing

--- parsers/parse_log.py
+++ parsers/parse_log.py
@@ -28,18 +28,18 @@ def parse_caffe(path, output_fields=['accuracy']):
                 iteration = int(l.split('Iteration ')[1].split(',')[0].split(' ')[0])
                 train_log.append({'iter':iteration})
                 if 'loss' in l:
-                    loss = float(l.split('loss = ')[-1])
+                    loss = float(l.split('loss = ')[-1].split(' ')[0])
                     train_log[-1]['loss'] = loss
             elif test:
                 found_field = False
                 for field in output_fields:
                     if field + " =" in l:
-                        accuracy = float(l.split('%s = '%field)[-1])
+                        accuracy = float(l.split('%s = '%field)[-1].split(' ')[0])
                         test_log[-1][field] = accuracy
                         found_field = True
                         break
                 if not found_field and 'loss =' in l:
-                    loss = float(l.split('loss = ')[-1].split(' ')[0])
+                    loss = float(l.split('loss = ')[-1].split(' ')[0].split(' ')[0])
                     test_log[-1]['loss'] = loss
 
     return train_log, test_log  
